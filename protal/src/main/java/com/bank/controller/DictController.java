9c0491dc9c51e6130b509053d96d4e0c368379a3
zhouxudong <854551809@qq.com>
2019-01-12 09:28:38 +0800

    银行系统

--- protal/src/main/java/com/bank/controller/DictController.java
+++ protal/src/main/java/com/bank/controller/DictController.java
@@ -2,7 +2,6 @@ package com.bank.controller;
 
 import com.bank.dubbo.dictService;
 import com.bank.pojo.Dict;
-import com.bank.utils.Data2layui;
 import com.bank.utils.Msg;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
