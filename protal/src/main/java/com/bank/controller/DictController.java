526c6081a5c2ee3ac32ba17d71c09fc47a3889a0
zhouxudong <854551809@qq.com>
2019-01-19 17:03:16 +0800

    银行系统

--- protal/src/main/java/com/bank/controller/DictController.java
+++ protal/src/main/java/com/bank/controller/DictController.java
@@ -8,32 +8,46 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @Controller
 @RequestMapping("/dict")
 public class DictController {
-@Autowired
-private dictService dict;
+    @Autowired
+    private dictService dict;
 
     /**
-     * @auther
      * @return
+     * @auther
      */
     @GetMapping("/select")
-    public Msg get(){
-        Msg data =new Msg();
-        List<Dict> list=dict.getAll();
+    public Msg get() {
+        Msg data = new Msg();
+        List<Dict> list=null;
+        //list = dict.getAll(new HashMap<String,Object>());
         data.setData(list);
         return data;
     }
-    @RequestMapping(value = "/getAll")
-    public List<Dict> getAll(@RequestParam(name = "dictCategory",defaultValue = "")String dictCategory ){
-        return dict.getAll();
-    }
 
+    @RequestMapping(value = "/getAll")
+    @ResponseBody
+    public List<Dict> getAll(@RequestParam(name = "dictCategory", defaultValue = "") String dictCategory,
+    @RequestParam(name = "dictId", defaultValue = "") String dictId
+    ) {
+        Map<String, Object> map = new HashMap<>();
+        if (dictCategory.trim().length() > 0) {
+            map.put("dictCategory", dictCategory);
+        }
+        if(dictId.trim().length()>0){
+            map.put("dictId",dictId);
+        }
+        return dict.getAll(map);
+    }
 
 
 }
