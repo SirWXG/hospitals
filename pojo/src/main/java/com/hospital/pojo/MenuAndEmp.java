7f99066e07653d9611ee0494533a3d276de4e809
wanxugang <1147798612@qq.com>
2019-02-13 16:24:51 +0800

    后台页面

--- pojo/src/main/java/com/hospital/pojo/MenuAndEmp.java
+++ pojo/src/main/java/com/hospital/pojo/MenuAndEmp.java
@@ -9,6 +9,15 @@ public class MenuAndEmp implements Serializable {
     private String empName;
     private String empUrl;
     private Integer empId;
+    private Integer orderNum;
+
+    public Integer getOrderNum() {
+        return orderNum;
+    }
+
+    public void setOrderNum(Integer orderNum) {
+        this.orderNum = orderNum;
+    }
 
     public String getEmpUrl() {
         return empUrl;
--- protal/src/main/java/com/hospital/controller/MenuController.java
+++ protal/src/main/java/com/hospital/controller/MenuController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping(value = "/menu")
@@ -21,11 +23,15 @@ public class MenuController {
     @RequestMapping(value = "/getMenu",method = RequestMethod.GET)
     public String getMenu(@RequestParam(name = "empId")String empId,Model model){
         List<MenuAndEmp> list = menuService.selectMenuByEmp(empId);
+        Map<String,Object> map = new HashMap<>();
         String empName = list.get(0).getEmpName();
         String empUrl = list.get(0).getEmpUrl();
-        model.addAttribute("empUrl",empUrl);
+        Integer orderNum = list.get(0).getOrderNum();
+        map.put("empName",empName);
+        map.put("empUrl",empUrl);
+        map.put("orderNum",orderNum);
         model.addAttribute("menu",list);
-        model.addAttribute("empName",empName);
+        model.addAttribute("emp",map);
         return "before_view";
     }
 }
